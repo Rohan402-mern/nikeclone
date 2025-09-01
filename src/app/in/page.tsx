cdf33779071d0f7e174e795c1ffd8c879b6c8028
Rohan402-mern <rohanpanday84548@gmail.com>
2025-09-01 06:41:09 -0700

    basic navbar setup done

---
+++ src/app/in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+     <img style={{width:'100%',}} src="https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_2456,c_limit/3249a845-8fec-4c90-bb28-a6826645034f/nike-just-do-it.jpg" alt="" />
+    </div>
+  )
+}
+
+export default page
